bfc8540b55971594d7336382ed8ec85bacc6acb4
Oscar99 <od_cortez@galileo.edu>
2014-12-23 11:59:35 -0600

    agregue lista de video

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
-var BetterListModel = function () {
-    this.itemToAdd = ko.observable("");
-    this.allItems = ko.observableArray(["Fries", "Eggs Benedict", "Ham", "Cheese"]); // Initial items
-    this.selectedItems = ko.observableArray(["Ham"]);                                // Initial selection
- 
-    this.addItem = function () {
-        if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)) // Prevent blanks and duplicates
-            this.allItems.push(this.itemToAdd());
-        this.itemToAdd(""); // Clear the text box
-    };
- 
-    this.removeSelected = function () {
-        this.allItems.removeAll(this.selectedItems());
-        this.selectedItems([]); // Clear selection
-    };
- 
-    this.sortItems = function() {
-        this.allItems.sort();
-    };
-};
- 
-ko.applyBindings(new BetterListModel());
+$(document).ready(function () {
+    //Knockout Test
+        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=VLLLSt653h2wwu3IeYhiL9Qwyg&part=snippet%2Cid&order=date&maxResults=4";
+        var viewModel = {};
+        $.getJSON(url, function (data) {
+            viewModel.model = data;
+            ko.applyBindings(viewModel);
+        });
+    });
