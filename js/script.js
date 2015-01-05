4b634b75d616e0ed4bf50a5872a1da4c493f5de0
Oscar99 <od_cortez@galileo.edu>
2015-01-05 12:06:16 -0600

    changes the filtres

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-$(document).ready(function () {
+stringUrl="";
+UrlData = "";
+viewCount = [];
+
+$(document).ready(function() {
+	$('.dropdown-toggle').dropdown()
+	});
+		$(document).ready(function($) {
+			$.ajax({
+				url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
+				dataType : "jsonp",
+				success : function(parsed_json) {
+				for (var i = 0; i <= 9; i++) {
+					if (i==9){
+						stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
+					}else{
+						stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
+					};
+				};
+				UrlData = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
+				console.log(UrlData)
+				$.ajax({
+				url: UrlData,
+				dataType : "jsonp",
+				success : function(parsed_jso) {
+					for (var e = 0; e<=9; e++) {
+						viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
+					};
+				console.log(viewCount)
+				}
+			});
+		}
+	});
+});
+/*$(document).ready(function () {
     //Knockout Test
         var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=VLLLSt653h2wwu3IeYhiL9Qwyg&part=snippet%2Cid&order=date&maxResults=4";
         var viewModel = {};
@@ -7,3 +41,4 @@ $(document).ready(function () {
             ko.applyBindings(viewModel);
         });
     });
+*/
