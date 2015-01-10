be625a51d3ba7adaa54cc29375525cca5320c7c7
Oscar <od_cortez@galileo.edu>
2015-01-09 18:12:03 -0600

    changes filtres with knockout

--- js/script.js
+++ js/script.js
@@ -1,44 +1,53 @@
 stringUrl="";
 UrlData = "";
 viewCount = [];
+likeCount = [];
+commentCount = [];
+id = [];
+title = [];
+description = [];
+image = [];
 
+/**
+ * Dropdown of filters 
+*/
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown()
-	});
-		$(document).ready(function($) {
-			$.ajax({
-				url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=10&format=json",
-				dataType : "jsonp",
-				success : function(parsed_json) {
-				for (var i = 0; i <= 9; i++) {
-					if (i==9){
-						stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
-					}else{
-						stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
-					};
-				};
-				UrlData = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
-				console.log(UrlData)
-				$.ajax({
-				url: UrlData,
-				dataType : "jsonp",
-				success : function(parsed_jso) {
-					for (var e = 0; e<=9; e++) {
-						viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
-					};
-				console.log(viewCount)
-				}
-			});
-		}
-	});
 });
-/*$(document).ready(function () {
-    //Knockout Test
-        var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=VLLLSt653h2wwu3IeYhiL9Qwyg&part=snippet%2Cid&order=date&maxResults=4";
-        var viewModel = {};
-        $.getJSON(url, function (data) {
-            viewModel.model = data;
-            ko.applyBindings(viewModel);
-        });
-    });
-*/
+
+$(document).ready(function($) {
+	$.ajax({
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=50&format=json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+	
+		for (var i = 0; i <= 49; i++) {
+			if (i==49){
+				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
+			}else{
+				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
+			}; /*End of for*/		
+		};/*Succes*/
+		UrlData = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
+		console.log(UrlData)
+		$.ajax({
+			url: UrlData,
+			dataType : "jsonp",
+			success : function(parsed_jso) {
+				for (var e = 0; e<=49; e++) {
+					title.push(parsed_jso["items"][e]["snippet"]["title"])
+					id.push(parsed_jso["items"][e]["id"])
+					description.push(parsed_jso["items"][e]["snippet"]["description"])		
+					viewCount.push(parsed_jso["items"][e]["statistics"]["viewCount"])
+					likeCount.push(parsed_jso["items"][e]["statistics"]["likeCount"])
+					commentCount.push(parsed_jso["items"][e]["statistics"]["commentCount"])
+					image.push(parsed_jso["items"][e]["snippet"]["thumbnails"]["default"]["url"])
+				};			
+			console.log(id)
+			console.log(title)
+			var initialData = []
+			for (var e = 0; e<=49; e++) {
+				initialData.push(({name:title[e], images:image[e], view:viewCount[e], like:likeCount[e], comment:commentCount[e]
+, ids:id[e]}))
+			};
+
