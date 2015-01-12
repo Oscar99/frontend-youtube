df9d4f530708906d2cdff1d0bcdd9ae6d4b86b47
Oscar <od_cortez@galileo.edu>
2015-01-11 19:33:19 -0600

    add filtres with knockout

--- js/script.js
+++ js/script.js
@@ -8,16 +8,11 @@ title = [];
 description = [];
 image = [];
 
-/**
- * Dropdown of filters 
-*/
-$(document).ready(function() {
-	$('.dropdown-toggle').dropdown()
-});
+
 
 $(document).ready(function($) {
 	$.ajax({
-		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCutAQ7OXuxEZ1Cw3ZPmPOZA&part=snippet&maxResults=50&format=json",
+		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCZJ7m7EnCNodqnu5SAtg8eQ&part=snippet&maxResults=50&format=json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
 	
@@ -26,8 +21,8 @@ $(document).ready(function($) {
 				stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
 			}else{
 				stringUrl = parsed_json["items"][i]["id"]["videoId"]+ "%2C+" +stringUrl
-			}; /*End of for*/		
-		};/*Succes*/
+			}; 
+		};
 		UrlData = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
 		console.log(UrlData)
 		$.ajax({
@@ -51,3 +46,80 @@ $(document).ready(function($) {
 , ids:id[e]}))
 			};
 
+var ViewModel = function(items) {
+    this.items = ko.observableArray(items);
+
+	this.sortByName = function() {
+        this.items.sort(function(a, b) {
+            return a.name < b.name ? -1 : 1;
+        });
+        $('.videoContent').slideUp();  $('.videoContent').slideDown();
+    };
+    	this.sortByName_z_a = function() {
+        this.items.sort(function(a, b) {
+            return a.name > b.name ? -1 : 1;
+        });
+    };
+
+	this.sortByLikes = function() {
+        this.items.sort(function(c, d) {
+            return c.like > d.like ? -1 : 1;
+        });
+    };
+	
+	this.sortByViews = function() {
+        this.items.sort(function(c, d) {
+            return c.view > d.view ? -1 : 1;
+        });
+    };
+    	
+
+	
+	this.sortByComments = function() {
+        this.items.sort(function(c, d) {
+            return c.comment < d.comment ? -1 : 1;
+        });
+    };     
+
+    	
+
+    this.gridOptions = {
+        data: this.items,
+        rowTemplate: "rowTmpl",
+        useKOTemplates: true,
+        height: 415,
+
+        columns: [ 
+            {
+                title: "Name"
+            },
+            {
+                title: "Image"   
+            },
+			{
+				title: "View Count"
+			},
+			{
+				title: "Like Count"
+			},
+			{
+				title: "Comment Count"
+			},
+        ],
+	pageable: {
+        
+        pageSize: 3,
+        buttonCount: 1
+      },
+    };
+};
+
+
+ko.applyBindings(new ViewModel(initialData));
+
+			}
+		}); 
+		} 
+
+	});
+});
